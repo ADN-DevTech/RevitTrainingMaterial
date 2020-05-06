827c13835b8630972dabb8c2a008fcd7ff89d3c9
NK29 <45867049+NK29@users.noreply.github.com>
2020-05-07 01:20:16 +0530

    2020 COMMIT

--- Labs/1_Revit_API_Intro/SourceVB/My Project/AssemblyInfo.vb
+++ Labs/1_Revit_API_Intro/SourceVB/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2019.0.0.0")>
-<Assembly: AssemblyFileVersion("2019.0.0.0")>
+<Assembly: AssemblyVersion("2020.0.0.0")>
+<Assembly: AssemblyFileVersion("2020.0.0.0")>
