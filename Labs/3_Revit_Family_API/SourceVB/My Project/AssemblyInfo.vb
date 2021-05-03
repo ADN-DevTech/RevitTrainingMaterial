6acbbd716dcdf0b653999e45e1c1e4989350376b
Jeremy Tammik <jeremy.tammik@autodesk.com>
2021-05-03 19:27:15 +0200

    integrated pull request #13 by @NK29 to eliminate deprecated API usage in family API labs

--- Labs/3_Revit_Family_API/SourceVB/My Project/AssemblyInfo.vb
+++ Labs/3_Revit_Family_API/SourceVB/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2022.0.0.1")>
-<Assembly: AssemblyFileVersion("2022.0.0.1")>
+<Assembly: AssemblyVersion("2022.0.0.2")>
+<Assembly: AssemblyFileVersion("2022.0.0.2")>
